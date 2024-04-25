9fb644456013a03cb60886119a1615d7bd222e65
Franco Ceballos <francoceballos@skiff.com>
2024-04-25 10:10:07 -0300

    modified:   index.php

--- index.php
+++ index.php
@@ -8,15 +8,26 @@
         $allRows = $rows->fetchAll(PDO::FETCH_OBJ);
  ?>
         <div class="row mt-5">
-        <?php foreach($allRows as $product): ?>
-            <div class="col-lg-4 col-md-6 col-sm-10 offset-md-0 offset-sm-1">
-                <div class="card mb-5">
-                    <img height="213px" src="<?php echo APPURL; ?>/images/<?php echo $product->image; ?>" class="card-img-top">
+        <?php foreach ($allRows as $product): ?>
+            <div class="col-md-3 offset-md-0 offset-sm-1">
+                <div class="card shadow rounded mb-5">
+                    <img 
+                        height="200px" 
+                        src="<?php echo APPURL; ?>/images/<?php echo $product->image; ?>" 
+                        class="card-img-top" 
+                    >
                     <div class="card-body">
                         <h5 class="d-inline"><b><?php echo $product->name; ?></b></h5>
-                        <h5 class="d-inline text-muted"><div>(<?php echo $product->price; ?>/item)</div></h5>
+                        <h5 class="d-inline text-muted">
+                            <div>(<?php echo $product->price; ?>$/item)</div>
+                        </h5>
                         <p><?php echo substr($product->description, 0, 100) . '...'; ?></p>
-                        <a href="<?php echo APPURL; ?>/shopping/single.php?id=<?php echo $product->id; ?>" class="btn btn-primary w-100 rounded my-2">More <i class="fas fa-arrow-right"></i></a>
+                        <a 
+                            href="<?php echo APPURL; ?>/shopping/single.php?id=<?php echo $product->id; ?>" 
+                            class="btn btn-primary w-100 rounded my-2"
+                        >
+                            More <i class="fas fa-arrow-right"></i>
+                        </a>
                     </div>
                 </div>
             </div>
