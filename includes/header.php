16b6ef240bd8359952f3674df685b631e5ceacb4
Franco Ceballos <francoceballos@skiff.com>
2024-05-10 19:12:58 -0300

    modified:   includes/header.php 	new file:   includes/style.css 	modified:   shopping/cart.php

--- includes/header.php
+++ includes/header.php
@@ -22,6 +22,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Bootstrap CSS -->
+    <link href="<?php echo APPURL; ?>/includes/style.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">    <script src="https://kit.fontawesome.com/5c5946fe44.js" crossorigin="anonymous"></script>
     <title>Bookstore</title>
 
@@ -29,72 +30,62 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     <script src="https://www.google.com/recaptcha/api.js?render=6LfNGtMpAAAAAP_JGn7iZHFG_xt0TUvjzCa9iXhM"></script>
-    <script>
-        $(document).ready(function() {
-            $("#submit").click(function() {
-                grecaptcha.ready(function() {
-                    grecaptcha.execute('6LfNGtMpAAAAAP_JGn7iZHFG_xt0TUvjzCa9iXhM', {action: 'submit'}).then(function(token) {
-                        $('#register-form').prepend('<input type="hidden" name="token" value="' + token + '">');
-                        $('#register-form').prepend('<input type="hidden" name="action" value="submit">');
-                        $('#register-form').submit();
-
-            });
-            });
-            })
-        })
-    </script>
 
 
     </head>
   <body style="background-color: #CDCDCD">
 
-    <nav class="navbar navbar-expand-lg" style="background-color: #0D0630; font-family: 'Fira Sans', sans-serif"" >
-    <div class="container" style="margin-top: none">
-        <a class="navbar-brand text-white" href=" <?php echo APPURL; ?>"><h2><i class="fa-solid fa-book"></i>  Bookstore</h2></a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-            <a class="nav-link active  text-white" aria-current="page" href="<?php echo APPURL; ?>">Home</a>
-            </li>
-            <li class="nav-item ">
-            <a class="nav-link  text-white " aria-current="page" href=" <?php echo APPURL; ?>/contact.php">Contact</a>
-            </li>
-            <li class="nav-item">
-            <a class="nav-link active  text-white" aria-current="page" href=" <?php echo APPURL; ?>/categories/index.php">Categories</a>
-            </li>
 
-            <?php if(isset($_SESSION['username'])): ?>
+    <nav class="navbar navbar-expand-lg" style="background-color: #0D0630; font-family: 'Fira Sans', sans-serif">
+        <div class="container">
+            <a class="navbar-brand text-white" href="<?php echo APPURL; ?>">
+                <h2><i class="fa-solid fa-book"></i> Bookstore</h2>
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active text-white" aria-current="page" href="<?php echo APPURL; ?>">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white" aria-current="page" href="<?php echo APPURL; ?>/contact.php">Contact</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active text-white" aria-current="page" href="<?php echo APPURL; ?>/categories/index.php">Categories</a>
+                    </li>
 
-                <li class="nav-item">
-                <a class="nav-link active  text-white" aria-current="page" href=" <?php echo APPURL; ?>/shopping/cart.php"><i class="fas fa-shopping-cart"></i> (<?php echo $getNumber->num_products; ?>)</a>
-                </li>
-                <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle  text-white" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    <?php echo $_SESSION['username']; ?>
-                </a>
-                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <li><a class="dropdown-item" href="<?php echo APPURL; ?>/users/orders.php?id=<?php echo $_SESSION['user_id']; ?>">My orders</a></li>
-                     <li><a class="dropdown-item" href="<?php echo APPURL; ?>/users/wishlist.php?id=<?php echo $_SESSION['user_id']; ?>">My wishlist</a></li> 
-                    <li><hr class="dropdown-divider"></li>
-                    <li><a class="dropdown-item" href="<?php echo APPURL;?>/auth/logout.php">Logout</a></li>
+                    <?php if(isset($_SESSION['username'])): ?>
+
+                        <li class="nav-item">
+                            <a class="nav-link active text-white" aria-current="page" href="<?php echo APPURL; ?>/shopping/cart.php">
+                                <i class="fas fa-shopping-cart"></i> (<?php echo $getNumber->num_products; ?>)
+                            </a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <?php echo $_SESSION['username']; ?>
+                            </a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <li><a class="dropdown-item" href="<?php echo APPURL; ?>/users/orders.php?id=<?php echo $_SESSION['user_id']; ?>">My orders</a></li>
+                                <li><a class="dropdown-item" href="<?php echo APPURL; ?>/users/wishlist.php?id=<?php echo $_SESSION['user_id']; ?>">My wishlist</a></li>
+                                <li><hr class="dropdown-divider"></li>
+                                <li><a class="dropdown-item" href="<?php echo APPURL; ?>/auth/logout.php">Logout</a></li>
+                            </ul>
+                        </li>
+
+                    <?php else: ?>
+
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="<?php echo APPURL; ?>/auth/login.php">Login</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="<?php echo APPURL; ?>/auth/register.php">Register</a>
+                        </li>
+
+                    <?php endif; ?>
                 </ul>
-                </li>
-
-            <?php else: ?>
-
-                <li class="nav-item">
-                    <a class="nav-link  text-white" href=" <?php echo APPURL; ?>/auth/login.php">Login</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link  text-white" href=" <?php echo APPURL; ?>/auth/register.php">Register</a>
-                </li>
-
-            <?php endif; ?>
-        </ul>
-       
+            </div>
         </div>
-    </div>
     </nav>
--- shopping/cart.php
+++ shopping/cart.php
@@ -24,116 +24,101 @@
 
 ?>
 
-<div class="container d-flex " style="font-family: 'Fira Sans', sans-serif">
+<div class="container d-flex" style="font-family: 'Fira Sans', sans-serif; margin-bottom: 100px; margin-top: 100px; ">
   <div class="col-md-11">
-    <div class="card rounded-4 shadow mb-5 mt-5" >
+    <div class="card rounded-4 shadow mb-5 mt-5 h-100" >
       <div class="card-body">
-        <div class="row g-0" >
-          <div class="col-lg-20">
-            <div class="p-5 mb-5 ">
-              <div class="d-flex justify-content-between align-items-center mb-5 ">
-                <h1 class="fw-bold text-black">Shopping Cart</h1>
-                <h6 class="mb-0 text-muted justify-content-end"><?php echo $getNumber->num_products; ?> items</h6>
-              </div>
-              <?php if(!empty($allProducts)): ?>
-              <table class="table table-light table-hover" height="100">
-                <thead>
-                  <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Image</th>
-                    <th scope="col">Name</th>
-                    <th scope="col">Individual price</th>
-                    <th scope="col">Quantity</th>
-                    <th scope="col">Total Price</th>
-                    <th scope="col">Delete</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <?php foreach($allProducts as $product): ?>
-                  <tr class="mb-4">
-                    <th scope="row"><?php echo $product->prod_id; ?></th>
-                    <td><img width="100" height="100" src="../images/<?php echo $product->prod_image; ?>" class="img-fluid rounded-3" alt="Cotton T-shirt"></td>
-                    <td><?php echo $product->prod_name; ?></td>
-                    <td class="price">
-                      <span class="prod_price"><?php echo $product->prod_price; ?></span>
-                      <span class="currency">$</span>
-                    </td>
-                    <td><input id="form1"       
-                    min="1" 
-                    name="quantity"
-                    value="<?php echo $product->prod_amount; ?>" 
-                    type="number" class="form-control form-control-sm prod_amount" 
-                    onchange="updateAmount(this, '<?php echo $product->id; ?>', '<?php echo $_SESSION['user_id']; ?>')" />
-                  </td>
-                    <td class="total_price"><?php echo $product->prod_price * $product->prod_amount; ?>$</td>
-                    <td>
-                      <!-- Button to trigger modal -->
-                      <button type="button" 
-                      class="btn btn-danger text-white btn-delete-item" 
-                      data-bs-toggle="modal" 
-                      data-bs-target="#deleteModal<?php echo $product->id; ?>">
-                        <i class="fa-solid fa-trash"></i>
-                      </button>
+        <?php if(!empty($allProducts)): ?>
+        <table class="table table-light table-hover" height="100">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Image</th>
+              <th scope="col">Name</th>
+              <th scope="col">Individual price</th>
+              <th scope="col">Quantity</th>
+              <th scope="col">Total Price</th>
+              <th scope="col">Delete</th>
+            </tr>
+          </thead>
+          <tbody class="table-group-divider h-100">
+            <?php foreach($allProducts as $product): ?>
+            <tr class="mb-4">
+              <th scope="row"><?php echo $product->prod_id; ?></th>
+              <td><img width="100" height="100" src="../images/<?php echo $product->prod_image; ?>" class="img-fluid rounded-3" alt="Cotton T-shirt"></td>
+              <td><?php echo $product->prod_name; ?></td>
+              <td class="price">
+                <span class="prod_price"><?php echo $product->prod_price; ?></span>
+                <span class="currency">$</span>
+              </td>
+              <td><input id="form1"      
+              min="1" 
+              name="quantity"
+              value="<?php echo $product->prod_amount; ?>" 
+              type="number" class="form-control form-control-sm prod_amount" 
+              onchange="updateAmount(this, '<?php echo $product->id; ?>', '<?php echo $_SESSION['user_id']; ?>')" /></td>
+              <td class="total_price"><?php echo $product->prod_price * $product->prod_amount; ?>$</td>
+              <td>
+                <!-- Button to trigger modal -->
+                <button type="button" 
+                class="btn btn-danger text-white btn-delete-item" 
+                data-bs-toggle="modal" 
+                data-bs-target="#deleteModal<?php echo $product->id; ?>">
+                  <i class="fa-solid fa-trash"></i>
+                </button>
 
-                      <!-- Modal -->
-                      <div class="modal fade" 
-                      id="deleteModal<?php echo $product->id; ?>" 
-                      tabindex="-1" 
-                      aria-labelledby="deleteModalLabel" 
-                      aria-hidden="true">
-                        <div class="modal-dialog">
-                          <div class="modal-content">
-                            <div class="modal-header">
-                              <h5 class="modal-title" 
-                              id="deleteModalLabel">Delete Confirmation</h5>
-                              <button type="button" 
-                              class="btn-close" 
-                              data-bs-dismiss="modal" 
-                              aria-label="Close"></button>
-                            </div>
-                            <div class="modal-body">
-                              Are you sure you want to delete this item?
-                            </div>
-                            <div class="modal-footer">
-                              <button type="button" 
-                              class="btn btn-secondary" 
-                              data-bs-dismiss="modal">Close</button>
-                              <button type="button" 
-                              value="<?php echo $product->id; ?>" 
-                              class="btn btn-danger text-white btn-delete ">
-                                Delete
-                              </button>
-                            </div>
-                          </div>
-                        </div>
+                <!-- Modal -->
+                <div class="modal fade" 
+                id="deleteModal<?php echo $product->id; ?>" 
+                tabindex="-1" 
+                aria-labelledby="deleteModalLabel" 
+                aria-hidden="true">
+                  <div class="modal-dialog">
+                    <div class="modal-content">
+                      <div class="modal-header">
+                        <h5 class="modal-title" 
+                        id="deleteModalLabel">Delete Confirmation</h5>
+                        <button type="button" 
+                        class="btn-close" 
+                        data-bs-dismiss="modal" 
+                        aria-label="Close"></button>
                       </div>
-                    </td>
-                  </tr>
-                  <?php endforeach ?>
-                </tbody>
-              </table>
-              <?php else: ?>
-              <div class="text-center p-5 fw-bold">
-                <h1><i class="fas fa-shopping-cart"></i></h1>
-                <h3>Cart is empty</h3>
-              </div>
-              <?php endif ?>
-            </div>
-            <div class=" d-flex justify-content-between">
-                <a href="<?php echo APPURL; ?>" class="btn btn-light text-white rounded-pill"  style="background-color: #384E77;"><i class="fas fa-arrow-left"></i> Continue Shopping</a>
-                <button class="delete-all delete-all-items btn btn-danger rounded-pill">Clear cart</button>
-            </div>
-          </div>
+                      <div class="modal-body">
+                        Are you sure you want to delete this item?
+                      </div>
+                      <div class="modal-footer">
+                        <button type="button" 
+                        class="btn btn-secondary" 
+                        data-bs-dismiss="modal">Close</button>
+                        <button type="button" 
+                        value="<?php echo $product->id; ?>" 
+                        class="btn btn-danger text-white btn-delete ">
+                          Delete
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </td>
+            </tr>
+            <?php endforeach ?>
+          </tbody>
+        </table>
+        <?php else: ?>
+        <div class="text-center p-5 fw-bold">
+          <h1><i class="fas fa-shopping-cart"></i></h1>
+          <h3>Cart is empty</h3>
         </div>
+        <?php endif ?>
       </div>
     </div>
   </div>
-  <div class="col-md-3 d-none d-md-block mt-5" style="margin-left: 50px; text-align: center;">
-    <div class="p-1" style="margin: auto; margin-top: 50px;">
-      <div class="card rounded-4 shadow mb-5 mt-5" style="text-align: center;">
-        <div class="card-body" style="text-align: center;">
+  <div class="col-md-3 d-none d-md-block mt-5" style="font-family: 'Fira Sans', sans-serif; margin-bottom: 100px; margin-top: 100px; ">
+    <div class="p-1">
+      <div class="card rounded-4 shadow mb-5 mt-5">
+        <div class="card-body">
           <h2 class="fw-bold mb-5 mt-2 pt-1" style="text-align: center;">Summary</h2>
-          <hr class="my-4" style="text-align: center;">
+          <hr class="my-4">
           <form method="POST" action="cart.php" style="text-align: center;">
             <div class="summary" style="text-align: center;">
               <div class="justify-content-between mb-5" style="text-align: center;">
@@ -153,7 +138,6 @@
   </div>
 </div>
 </div>
-
 
 <?php require '../includes/footer.php'; ?>
 
