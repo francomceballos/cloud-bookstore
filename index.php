763358405746adeecc4e4b3e00d61676b6ae37be
Franco Ceballos <francoceballos@skiff.com>
2024-05-13 10:55:25 -0300

    modified:   includes/style.css 	modified:   index.php 	modified:   shopping/cart.php

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
     <div class="row mt-5" style="margin-bottom: 100px">
         <?php foreach ($allRows as $product): ?>
             <div class="col-lg-4 col-md-6 col-sm-10 offset-md-0 offset-sm-1 mb-4">
-                 <div class="card h-100 rounded-4 shadow border-0">
+                 <div class="card h-100 rounded-4 shadow border-0 animate-fade-in">
                     <img 
                             class="card-img-top "
                             src="<?php echo APPURL; ?>/images/<?php echo htmlspecialchars($product->image, ENT_QUOTES, 'UTF-8'); ?>" 
@@ -26,7 +26,7 @@
                             <p class="text-truncate" style="max-width: 100%;"><?php echo htmlspecialchars(substr($product->description, 0, 200), ENT_QUOTES, 'UTF-8'); ?></p>
                             <a 
                             href="<?php echo APPURL; ?>/shopping/single.php?id=<?php echo htmlspecialchars($product->id, ENT_QUOTES, 'UTF-8'); ?>" 
-                            class="btn btn-dark w-100 rounded-pill my-2 btn-outline-light" style="background-color: #0D0630;"
+                            class="btn btn-dark w-100 rounded-pill my-2" style="background-color: #0D0630;"
                         >
                             See more</i>
                         </a>
